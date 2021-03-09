5de6b1035ccf024aac77817e6f180ee5f25f7b1f
nadirkorkut <korkutnadir@outlook.com>
2021-03-09 18:38:40 +0300

    DataAccess içerisine Abstract ve Concrete klasörleri eklendi ve core katmanına DataAccess klasörü eklenerek IEntityRepository generic olarak yazılarak eklenecek başka entitylerin de kullanabileceği bir yapıda oluşturuldu. IPersonDal bu repositoryi kalıtım aldı. Daha sonra entityframework için manage nuget package ile entity framework paketi yüklendi. Migration alındı. Ef klasörüne IPersonDal interfacenin somutu yazıldı. Daha sonra eklenebilecek varlıklar göz önüne alınarak Core katmanında bir resositorybase yazıldı ve EfPersonDal bu base classtan kalıtım aldı. EfEntityRepositoryBase veritabanı işlemlerimizi gerçekleştiriyor fakat bu da generic yapıda yazılarak değişebilecek durumlara göre kodlandı.

---
+++ DataAccess/Concrete/EntityFramework/Context/PersonDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class PersonDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=NADIR; Initial Catalog=PersonDb; Integrated Security=TRUE;");
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfPersonDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Context;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfPersonDal:EfEntityRepositoryBase<Person,PersonDbContext>,IPersonDal
+    {
+    }
+}
