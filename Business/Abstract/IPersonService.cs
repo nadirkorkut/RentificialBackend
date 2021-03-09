ec321679638b88ce2d6e2adc285a5a2b453d7562
nadirkorkut <korkutnadir@outlook.com>
2021-03-09 19:09:30 +0300

    Business katmanında yapılacak iş kodlarının soyutu IPersonService classında yazıldı. Somutu da PersonMenager classında IPersonService implemente edilerek yazıldı.

---
+++ Business/Abstract/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IPersonService
+    {
+        List<Person> GetList();
+        Person GetPersonById(int id);
+        void Add(Person person);
+        void Delete(Person person);
+        void Update(Person person);
+    }
+}
---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class PersonManager : IPersonService
+    {
+        private IPersonDal _personDal;
+        public PersonManager(IPersonDal personDal)
+        {
+            _personDal = personDal;
+        }
+        public void Add(Person person)
+        {
+            _personDal.Add(person);
+        }
+
+        public void Delete(Person person)
+        {
+            _personDal.Delete(person);
+        }
+
+        public List<Person> GetList()
+        {
+            return _personDal.GetList().ToList();
+        }
+
+        public Person GetPersonById(int id)
+        {
+            return _personDal.Get(p => p.Id == id);
+        }
+
+        public void Update(Person person)
+        {
+            _personDal.Update(person);
+        }
+    }
+}
