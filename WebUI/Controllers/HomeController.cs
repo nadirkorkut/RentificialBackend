e35a08bcaf1e9fc57549ae0a6352585a45a0e88b
nadirkorkut <korkutnadir@outlook.com>
2021-03-11 13:48:37 +0300

    Proje Asp.net core mvc ile UI katmanı eklendi. Home Controllerda PersonManager ve EfPersonDal instance alınarak sadece tüm verileri listeleme metodumuzu çağırdık. View index içerisine de kullanacağımız entity modeli yazdım ve basit bir tablo içerisinde kullanıcıya kişilerin adını soyadını ve telefon numaralarını listeledim.

---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        PersonManager _personManager = new PersonManager(new EfPersonDal());
+        public IActionResult Index()
+        {
+            List<Person> persons = _personManager.GetList();
+            return View(persons);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
